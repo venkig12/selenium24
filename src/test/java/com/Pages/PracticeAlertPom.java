10f159b03374c30fe7ca6a72cb64cad71d387972
sjkoora <swapna374@gmail.com>
2024-05-01 20:03:31 -0500

    assignments

---
+++ src/test/java/com/Pages/PracticeAlertPom.java
@@ -0,0 +1,36 @@
+package com.Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PracticeAlertPom {
+	WebDriver driver;
+	
+	public PracticeAlertPom(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(name ="alert" )
+	public WebElement NormalAlert;
+	
+	@FindBy(name="confirmation")
+	public WebElement ConfirmationAlert;
+	
+	@FindBy(name="prompt")
+	public WebElement PromptBoxAlert;
+	
+	@FindBy(name="download")
+	public WebElement FileDownloadAlert;
+	
+	@FindBy(xpath="//a[text()='Download PDF File']")
+	public WebElement ChooseFileButton ;
+	
+	public void ClickOnNormalAlert() {
+		NormalAlert.click();}
+	
+	
+	
+}
