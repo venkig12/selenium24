128908df3d2bfd970459a7f34aaf687b1e9ce379
sjkoora <swapna374@gmail.com>
2024-05-03 21:42:37 -0500

    Changed the allignment

---
+++ src/test/java/com/Pages/MouserOperationsPOM.java
@@ -0,0 +1,37 @@
+package com.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MouserOperationsPOM {
+	WebDriver driver;
+	
+	public MouserOperationsPOM(WebDriver driver){
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//span[text()='right click me']")
+	public WebElement rightClickMeButton;
+	
+	@FindBy(xpath="//span[text()='Copy']")
+	public WebElement copyOption;
+	
+	@FindBy(tagName = "iframe")
+	public WebElement iframe;
+	
+	@FindBy(xpath="//span[contains(text(),'Double click')]/preceding-sibling::div")
+	public WebElement doubleClickBox;
+	
+	@FindBy(id="draggable")
+	public WebElement Draggable;
+	
+	@FindBy(id="droppable")
+	public WebElement Droppable;
+
+	@FindBy(xpath="//a[text()='Accept']")
+	public WebElement AcceptOutSideframe;
+	
+}
